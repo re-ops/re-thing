786e8f4eb22ddde857d080a371bde3ee68a42024
ronen <narkisr@gmail.com>
2021-02-06 00:08:55 +1100

    clearing unused values and ns

--- src/re_thing/log.clj
+++ src/re_thing/log.clj
@@ -1,8 +1,6 @@
 (ns re-thing.log
   (:require
    [re-share.log :as log]
-   [taoensso.timbre.appenders.3rd-party.rolling :refer (rolling-appender)]
-   [taoensso.timbre.appenders.core :refer (println-appender)]
    [taoensso.timbre  :as timbre :refer (merge-config! set-level! refer-timbre)]))
 
 (refer-timbre)
@@ -13,7 +11,7 @@
     - steam collect logs
     - log level
   "
-  [& {:keys [interval level] :or {interval 10 level :info}}]
+  [& {:keys [level] :or {interval 10 level :info}}]
   (log/setup "re-thing" ["io.moquette.broker.*"] ["re-thing.output"])
   (set-level! level))
 
