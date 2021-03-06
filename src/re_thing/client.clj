75dc0e69f9cea61f5a836984385354a866d5f096
ronen <narkisr@gmail.com>
2021-03-06 17:28:59 +1100

    Adding reconnection loop

--- src/re_thing/client.clj
+++ src/re_thing/client.clj
@@ -1,5 +1,6 @@
 (ns re-thing.client
   (:require
+   [mount.core :as mount]
    [re-share.config.core :refer  (get!)]
    [taoensso.timbre :refer (refer-timbre)]
    [mount.core :as mount :refer (defstate)])
@@ -15,14 +16,7 @@
 
 (def handlers (atom {}))
 
-(defn subscriber []
-  (proxy [MqttCallback] []
-    (connectionLost [throwable]
-      (error "lost connection to broker" throwable))
-    (messageArrived [topic message]
-      ((@handlers topic) message))
-    (deliveryComplete [token]
-      (debug "delivered message" {:token token}))))
+(declare subscriber)
 
 (defn start- []
   (let [{:keys [host client-id auth]} (get! :mosquitto)
@@ -46,13 +40,41 @@
   :start (start-)
   :stop (stop- client))
 
-(defn log-message
-  [message]
-  (info (String. ^bytes message)))
-
 (defn subscribe [q f]
   (swap! handlers assoc q f)
   (.subscribe client (into-array String [q]) (int-array [2])))
+
+(defn reconnect
+  "re-connection loop for 1 hour in case we lost connection"
+  []
+  (let [i (atom 60)]
+    (while (> @i 0)
+      (try
+        (info "trying to reconnect to mqtt server")
+        (mount/stop #'client)
+        (mount/start #'client)
+        (info "client started")
+        (doseq [[q f] @handlers]
+          (subscribe q f))
+        (reset! i 0)
+        (catch Exception e
+          (swap! i dec)
+          (error "Failed to reconnect will retry again in 60 sec" e)
+          (Thread/sleep (* 60 1000)))))))
+
+(defn subscriber []
+  (proxy [MqttCallback] []
+    (connectionLost [throwable]
+      (error "lost connection to broker" throwable)
+      (reconnect))
+    (messageArrived [topic message]
+      ((@handlers topic) message))
+    (deliveryComplete [token]
+      (debug "delivered message" {:token token}))))
+
+(defn log-message
+  [message]
+  (info (String. ^bytes message)))
 
 (defn publish [q m]
   (let [message (doto (MqttMessage. (.getBytes m)) (.setQos (get! :mosquitto :qos)))]
